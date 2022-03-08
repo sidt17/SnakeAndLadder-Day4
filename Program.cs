ec09236372d61cff0abee655d2376654d8e2c5ae
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 14:47:50 +0530

    Call in main method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Snakeandladdergame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder Game");
+            Startposition.Singleplayer();
         }
     }
 }
