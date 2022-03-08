2920fa47ec3781adc0ea82fff28bfb4284d3993e
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 14:43:45 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snakeandladdergame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+    }
+}
