0044b53f29b58d1358f6083f954481f96e96cc56
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 16:11:10 +0530

    Repeat till reaches 100

---
+++ Winningposition.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace Snakeandladdergame
+{ 
+    public class Winningposition
+    {
+       public static void position100()
+       {
+         
+        
+
+        int position = 0;
+        int Noplay = 0;
+        int ladder = 1;
+        int snake = 2;
+
+            while(position<100)
+            {
+                Random random = new Random();
+                int number = random.Next(1, 7);
+                int condition = random.Next(3);
+
+                switch (condition)
+                {
+                    case 0:
+                        Console.WriteLine("No Play and position is " + position);
+                        break;
+                    case 1:
+                        position = position + number;
+                        Console.WriteLine("You got a Ladder and position is " + number);
+                        break;
+                    case2:
+                        position = position - number;
+                        Console.WriteLine("You are on Snake and position is " + number);
+                }
+
+
+
+
+            }
+
+           
+            
+            Console.WriteLine($"Final Position Of Player is " +position);
+
+
+
+        }
+
+        
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
