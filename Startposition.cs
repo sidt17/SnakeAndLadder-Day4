a9bede264ed695a10ab1c7bdf74b0815f8c1f471
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 14:49:31 +0530

    Single Player

---
+++ Startposition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Snakeandladdergame
+{
+    public class Startposition
+    {
+        public static void Singleplayer()
+        {
+            int startposition = 0;
+           
+            Console.WriteLine("Please enter your name :");
+            string input=Console.ReadLine();
+
+            Console.WriteLine("Your current position is :" + startposition);
+        }
+    }
+    
+
+    
+}
