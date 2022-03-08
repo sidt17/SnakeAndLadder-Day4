a6845a51eb96216efc83d66022008c04e2d5d842
Siddhant <santoshshrivastava39@gmail.com>
2022-03-08 14:55:33 +0530

    Player rolls the die

---
+++ Diceroll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Snakeandladdergame
+{
+    public class Diceroll
+    {
+        public static void Player()
+        {
+            Console.WriteLine("Please enter your name :");
+            string input = Console.ReadLine();
+
+            Random random = new Random();
+            int number = random.Next(1, 7);
+
+            
+           Console.WriteLine("The number appeared is:" +number);
+            
+
+            
+        }
+    }
+}
